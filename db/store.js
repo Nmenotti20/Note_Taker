09a541e5956dd6e06069cb0ae4b7a243f0adf15b
Nickolas Menotti <nmenotti@cox.net>
2020-12-18 08:09:36 -0800

    created routes

---
+++ db/store.js
@@ -0,0 +1,16 @@
+const util = require("util");
+const fs = require("fs");
+
+const uuidv1 = require("uuid/v1");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// create struction of objects I am going to store
+
+
+// creat a class for dynamic object creation
+
+store
+
+
+// functions to wrte a note
