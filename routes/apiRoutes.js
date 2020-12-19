16e831710e79dae9a4bc831d4e9d3580c6bb5aaa
Nickolas Menotti <nmenotti@cox.net>
2020-12-18 17:42:54 -0800

    added notation to apiRoutes and htmlRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
+// Import files and modules
 const router = require("express").Router();
 const store = require("../db/store");
 
+// Establish apiRoutes 
 router.get("./notes", (req, res) => {
     store
         .getNotes()
@@ -8,6 +10,7 @@ router.get("./notes", (req, res) => {
         .catch((err) => res.status(500).json(err))
 });
 
+// API post request
 router.post("./notes", (req, res) => {
     store
         .addNote(req.body)
@@ -22,6 +25,5 @@ router.delete("./notes:id", (req, res) => {
         .catch((err) => res.status(500).json(err))
 });
 
-
+// Export the module
 module.exports = router
-
--- routes/hmtlRoutes.js
+++ routes/hmtlRoutes.js
@@ -1,13 +1,20 @@
+// Import files and modules
 const { Router } = require("express");
 const path = require("path");
 const router = require("express").Router();
 
+// Establish htmlRoutes
+// Get request for notes to return the notes to 'notes.html' file located inside the 'public' directory
 router.get("./notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
 
+// Get request for all other requests to return to the 'index.html' file located inside the 'public' directory
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
+// Export the module
 module.exports = router
+
+
